2fa71fe0bdad28e6ed6a962eb2d427424f0b6bd2
teqodoo <ravisk1514@gmail.com>
2019-01-17 17:50:31 +0530

    try

--- app.js
+++ app.js
@@ -75,9 +75,9 @@ app.use("/admin", adminRoutes);
 app.use("/user", userRoutes);
 app.use("/hotels", hotelRoutes);
 
-// app.listen(3000, () => {
-//     console.log('###################')
-//     console.log("The Hotel website server has been started", 3000);
-// });
+app.listen(process.env.PORT || 3000, () => {
+    console.log('###################')
+    console.log("The Hotel website server has been started", 3000);
+});
 
-app.listen(process.env.PORT || 3000);
+// app.listen(process.env.PORT || 3000);
