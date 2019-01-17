78a9181d6c4aaa7a95e8a2a65b575cd93b75c5af
teqodoo <ravisk1514@gmail.com>
2019-01-17 18:57:23 +0530

    heroku master done

--- routes/index.js
+++ routes/index.js
@@ -78,34 +78,11 @@ router.post("/register", function (req, res) {
 });
 
 
-// router.post("/login", function (req, res) {
-//     var newUser = new User({ username: req.body.username });
-//     User.register(newUser, req.body.password, function (err, user) {
-//         if (err) {
-//             req.flash("error", err.message);
-//             res.redirect("register");
-//         }
-//         passport.authenticate("local")(req, res, function () {
-//             req.flash("success", "Welcome to LakeSide :" + user.username);
-//             res.redirect("/hotels");
-//         });
-//     });
-// });
-
-
 // Show login form
 router.get("/login", function (req, res) {
     res.render("login");
 });
 
-// router.post("/login", passport.authenticate("local",
-//     {
-//         successRedirect: "/hotels",
-//         failureRedirect: "/login",
-//     }), function (req, res) {
-//         // req.flash("success", "Welcome to LakeSide :" + user.username);
-//         // res.redirect("/hotels");
-//     });
 router.post("/login", passport.authenticate("local",
     {
         failureRedirect: "/login",
@@ -135,7 +112,7 @@ router.get('/raise', midddleware.isLoggedIn, (req, res, next) => {
     res.render("raise");
 });
 
-router.get('/feedback', midddleware.isLoggedIn, (req, res, next) => {
+router.get('/feedback', (req, res, next) => {
     res.render("feedback");
 });
 
